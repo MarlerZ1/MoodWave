86e4c9955e1e904225978e16a507fb52f9c40bd9
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-10-26 21:19:54 +0300

    Add chats filter by user id

--- chats/views.py
+++ chats/views.py
@@ -2,33 +2,43 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import ListView
 
 from chats.models import Chat, UserInChat, CHAT
+from common.exceptions.exceptions import IncorrectDialoguePeopleNumber
 
 
 # Create your views here.
 
 class ChartsView(ListView):
-    model = Chat
+    model = UserInChat
     template_name = 'chats/chart_list.html'
 
     def get_queryset(self):
-        queryset = super(ChartsView, self).get_queryset()
+        queryset = super(ChartsView, self).get_queryset().filter(user_id=self.request.user.id)
 
         chats = []
+        for relationship in queryset:
+            chats += [relationship.chat]
 
-        for chat in queryset:
+
+        chats_info = []
+
+        for chat in chats:
             if chat.format == CHAT:
                 try:
-                    chats += [{'name': chat.chatinfo.name, 'logo': chat.chatinfo.logo, 'format': 'chat'}]
+                    chats_info += [{'name': chat.chatinfo.name, 'logo': chat.chatinfo.logo, 'format': 'chat'}]
                 except ObjectDoesNotExist as e:
                     raise e
             else:
+
                 users_in_chat = UserInChat.objects.filter(chat_id=chat.id)
+
+                if len(users_in_chat) != 2:
+                    raise IncorrectDialoguePeopleNumber()
 
                 if self.request.user.id == users_in_chat[0].user.id:
                     another_user = users_in_chat[1].user
                 else:
                     another_user = users_in_chat[0].user
 
-                chats += [{'name': another_user.first_name + " " + another_user.last_name, 'logo': another_user.logo ,'format': 'dialogue'}]
+                chats_info += [{'name': another_user.first_name + " " + another_user.last_name, 'logo': another_user.logo ,'format': 'dialogue'}]
 
-        return chats
+        return chats_info
