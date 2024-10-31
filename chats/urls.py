b7cb414d4f266d792ff1a479341dda3ca9763bc7
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-10-31 14:30:13 +0300

    Add chat page with messages

--- chats/urls.py
+++ chats/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from chats.views import ChartsView
+from chats.views import ChatsView, MessagesView
 
 app_name = "chats"
 
 urlpatterns = [
-    path('chat_list/', ChartsView.as_view(), name="chat_list"),
+    path('chat_list/', ChatsView.as_view(), name="chat_list"),
+    path('chat/<chat_id>', MessagesView.as_view(), name="chat"),
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,19 +1,18 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import ListView
-from pyexpat.errors import messages
 
-from chats.models import Chat, UserInChat, CHAT, Message
+from chats.models import UserInChat, CHAT, Message
 from common.exceptions.exceptions import IncorrectDialoguePeopleNumber
 
 
 # Create your views here.
 
-class ChartsView(ListView):
+class ChatsView(ListView):
     model = UserInChat
-    template_name = 'chats/chart_list.html'
+    template_name = 'chats/chat_list_page/chat_list.html'
 
     def get_queryset(self):
-        queryset = super(ChartsView, self).get_queryset().filter(user_id=self.request.user.id)
+        queryset = super(ChatsView, self).get_queryset().filter(user_id=self.request.user.id)
 
         chats = []
         for relationship in queryset:
@@ -27,7 +26,7 @@ class ChartsView(ListView):
 
             if chat.format == CHAT:
                 try:
-                    chats_info += [{'name': chat.chatinfo.name, 'logo': chat.chatinfo.logo, 'message': message, 'format': 'chat'}]
+                    chats_info += [{'name': chat.chatinfo.name, 'logo': chat.chatinfo.logo, 'message': message, 'format': 'chat', 'chat_id': chat.id}]
                 except ObjectDoesNotExist as e:
                     raise e
             else:
@@ -42,6 +41,25 @@ class ChartsView(ListView):
                 else:
                     another_user = users_in_chat[0].user
 
-                chats_info += [{'name': another_user.first_name + " " + another_user.last_name, 'logo': another_user.logo ,'message': message, 'format': 'dialogue'}]
+                chats_info += [{'name': another_user.first_name + " " + another_user.last_name, 'logo': another_user.logo ,'message': message, 'format': 'dialogue', 'chat_id': chat.id}]
 
         return chats_info
+
+class MessagesView(ListView):
+    model = Message
+    template_name = 'chats/chat/chat.html'
+
+    def get_queryset(self):
+        queryset = super(MessagesView, self).get_queryset()
+
+        users_in_chat = UserInChat.objects.filter(chat_id=self.kwargs['chat_id'])
+
+        queryset = queryset.filter(user_id__in=[user.user_id for user in users_in_chat], chat_id=self.kwargs['chat_id'])
+
+        messages = [{
+            'from_me': message.user_id == self.request.user.id,
+            'name': message.user.first_name + " " + message.user.last_name,
+            'text': message.text
+        } for message in queryset]
+
+        return messages
