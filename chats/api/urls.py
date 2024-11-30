82dd9083c8709062f59dae20bb671ccddd07e28b
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-11-30 21:14:04 +0300

    Add DRF view to chat page

--- chats/api/urls.py
+++ chats/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from chats.api.views import ChatListApiView
+from chats.api.views import ChatListApiView, MessagesApiView
 from chats.web.views import ChatsView, MessagesView
 
 app_name = "chats_api"
 
 urlpatterns = [
     path('chat_list/', ChatListApiView.as_view(), name="chat_list"),
+    path('chat/<chat_id>', MessagesApiView.as_view(), name="chat"),
 ]
--- chats/api/views.py
+++ chats/api/views.py
@@ -3,8 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from chats.business_logic import ChatsListPageBL
-from chats.models import UserInChat
+from chats.business_logic import ChatsListPageBL, MessagesPageBL
+from chats.models import UserInChat, Message
 
 
 class ChatListApiView(APIView):
@@ -12,3 +12,10 @@ class ChatListApiView(APIView):
 
     def get(self, request, *args, **kwargs):
         return Response(ChatsListPageBL.get_chats(UserInChat.objects.all(), request.user.id), status=status.HTTP_200_OK)
+
+
+class MessagesApiView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        return Response(MessagesPageBL.get_formated_messages(Message.objects.all(), kwargs['chat_id'], request.user.id))
